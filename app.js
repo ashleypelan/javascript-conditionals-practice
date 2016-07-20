f67bbabe8fb8cffb848491c050cca5890716b6cd
Ashley Pelan <ashley.pelan@gmail.com>
2016-07-20 08:27:42 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var numberArray = [];
+
+var buttonClick = document.getElementById('submitButton');
+document.getElementById('numberInput').push(numberArray);
+
+
+
+if (numberArray[3] == '-' && numberArray[7] == '-') {
+  alert(True);
+} else {
+  alert(False);
+}
